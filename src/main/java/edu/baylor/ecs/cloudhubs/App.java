e7babefe56074a49b91cb6bbb3704662d7342f53
mr-noah <noah134567@gmail.com>
2021-06-08 15:06:12 -0500

    added compile dependencies required to use github API into pom

--- src/main/java/edu/baylor/ecs/cloudhubs/App.java
+++ src/main/java/edu/baylor/ecs/cloudhubs/App.java
@@ -1,13 +1,24 @@
 package edu.baylor.ecs.cloudhubs;
 
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+
+        GitHub github = GitHub.connect();
+
+        GHRepository repo = github.createRepository(
+                "new-repository","this is my new repository",
+                "https://www.kohsuke.org/",true/*public*/);
+        repo.addCollaborators(github.getUser("abayer"),github.getUser("rtyler"));
+        repo.delete();
     }
 }
